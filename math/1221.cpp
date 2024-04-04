77388306115d37c44eabf1587a6f43a480af7295
Akemi <akemikoba1903@gmail.com>
2024-04-04 19:02:18 -0300

    exercises

--- math/1221.cpp
+++ math/1221.cpp
@@ -11,9 +11,7 @@ int main() {
     while(cases--){
         cin >> n;
         int cont=0;
-        //cout << "RAIZ QUADRADA" << sqrt(n) << endl;
         for(int i=2;  i<=sqrt(n); i++){
-            cout << "N ATUAL " << n << endl;
             if(n%i==0){
                 cont++;
             }
